80e8ab23188e776f65e08fde1d023992bdc99c0d
zethembemvundla <zethembemvundla01@gmal.com>
2023-08-16 09:15:11 -0700

    POP

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,8 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	       	else if  (n == 0)
+	}
+	else if  (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
 	}
 }
