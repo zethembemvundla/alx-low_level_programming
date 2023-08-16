0dad6e0afc8846839fc1fd061dccf95470ec1c6b
zethembemvundla <zethembemvundla01@gmal.com>
2023-08-16 08:58:22 -0700

    oi

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,11 @@
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  */
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
-
 	i = 0;
-
 	while (i < 10)
 	{
 		ch = 'a';
