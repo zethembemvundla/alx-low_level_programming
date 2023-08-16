eb9a071c77dc2232fa8fdbc7c1683e00d3e5d27c
zethembemvundla <zethembemvundla01@gmal.com>
2023-08-16 09:44:41 -0700

    22

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer 
- * starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
@@ -30,4 +29,3 @@ void jack_bauer(void)
 	}
 	}
 }
-
